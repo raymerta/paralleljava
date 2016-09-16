d279cab19785d3d3c81fe2fc9a09d0b3c0cd59a5
Radita Liem <raymerta@gmail.com>
2016-09-16 23:21:29 +0700

    java shell updated

--- examples/RunExamples.sh
+++ examples/RunExamples.sh
@@ -4,13 +4,10 @@
 # Script needs to be in the examples folder
 module load jdk-1.8.0_25
 module load java-1.8.0_40
+module load openmpi-1.8.4
 
-export MPIDIR=OpenMPI
-export PATH=$HOME/$MPIDIR/install/bin:$PATH
-export LD_LIBRARY_PATH=$HOME/$MPIDIR/install/lib:$LD_LIBRARY_PATH
+srun -N 1 -n 1 -t 0:10:00 --mem=16384 mpijavac Pi.java
+srun -N 1 -n 1 -t 0:10:00 --mem=16384 mpijavac MatrixPar.java
 
-srun -N 1 -n 1 -t 0:10:00 --mem=16384 $HOME/$MPIDIR/install/bin/mpijavac Pi.java
-srun -N 1 -n 1 -t 0:10:00 --mem=16384 $HOME/$MPIDIR/install/bin/mpijavac MatrixPar.java
-
-srun -N 1 -n 10 -t 0:10:00 --mem=16384 $HOME/$MPIDIR/install/bin/mpirun -np 10 java Pi
-srun -N 1 -n 10 -t 0:10:00 --mem=16384 $HOME/$MPIDIR/install/bin/mpirun -np 10 java MatrixPar
+srun -N 1 -n 10 -t 0:10:00 --mem=16384 mpirun -np 10 java Pi
+srun -N 1 -n 10 -t 0:10:00 --mem=16384 mpirun -np 10 java MatrixPar
